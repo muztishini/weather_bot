b113b5d52f0d36164208c80f2a916c98860aaf7e
muztishini <111636144+muztishini@users.noreply.github.com>
2024-10-12 16:37:05 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
-open_weather_token = "3bfa73efe7e00b90cde04629c4b26865"
-# tg_bot_token = "5030675315:AAEOD3tBYRMDBkWPA8WSjhiChjBG-lL55Yg"
-tg_bot_token = "7032325093:AAFYZhSxUfdKypnqOVbW76QoAlsLchbA_Ts"
+open_weather_token = ""
+tg_bot_token = ""
